fee79ba518cf6b51112d9da1f94da5068e607769
Chad KS Shikuambi <chadshikuambi@gmail.com>
2026-09-03 13:04:54 +0200

    Week3 Submission

---
+++ Week3/lab1.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main(){
+    //Declaring variables
+    double salary=0.00;
+    double housingAllowance=0.00;
+    double transportAllowance=0.00;
+    double taxAmount=0.00;
+
+    double grossSalary=0.00;
+    double netSalary=0.00;
+
+    //1. Ask user for salary
+    printf("Enter the Salary");
+    scanf("%lf", &salary);
+
+    //2. Ask user for housing allowance
+    //3. Ask user for transport allownace
+    //4. Ask user for Tax amount
+    //5. Calculate the Gross
+    //6. Display the Gross
+    //7. Calculate the net
+    //8. Display the net
+    return 0;
+}
