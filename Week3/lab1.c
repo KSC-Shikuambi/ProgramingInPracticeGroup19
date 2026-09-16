5b4c522f915ac62694589c241e9f57033b22a731
Chad KS Shikuambi <chadshikuambi@gmail.com>
2026-09-16 10:07:16 +0200

    Week3-lab1

--- Week3/lab1.c
+++ Week3/lab1.c
@@ -1,25 +1,36 @@
 #include <stdio.h>
-
 int main(){
-    //Declaring variables
-    double salary=0.00;
+    //This is the code for lab 1
+    //Declare Variables
+    double basicSalary=0.00;
     double housingAllowance=0.00;
     double transportAllowance=0.00;
     double taxAmount=0.00;
-
     double grossSalary=0.00;
     double netSalary=0.00;
 
-    //1. Ask user for salary
-    printf("Enter the Salary");
-    scanf("%lf", &salary);
+    //1.Prompt user for Basic Salary
+    printf("Enter Basic Salary");
+    scanf("%lf", &basicSalary);
 
-    //2. Ask user for housing allowance
-    //3. Ask user for transport allownace
-    //4. Ask user for Tax amount
-    //5. Calculate the Gross
-    //6. Display the Gross
-    //7. Calculate the net
-    //8. Display the net
+    //2. Prompt user for Housing Allowance
+    printf("Enter Housing Allowance");
+    scanf("%lf", &housingAllowance);
+
+    //3. Prompt user for Transport Allowance
+    printf("Enter Transport Allowance");
+    scanf("%lf", &transportAllowance);
+
+    //4. Prompt user for Tax Amount
+    printf("Enter tax amount");
+    scanf("%lf", &taxAmount);
+
+    //5. Displaying Gross Salary
+    grossSalary=basicSalary +transportAllowance +housingAllowance;
+    printf("This is the Gross Salary: %lf", grossSalary);
+
+    //6. Displaying Net Salary
+    netSalary=grossSalary-taxAmount;
+    printf("This is the net salary: %lf", netSalary);
     return 0;
 }
